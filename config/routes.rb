edc88c2d96aaa076de2984e7ef04d355199787b7
Megan Carlton <J273281@tafe.wa.edu.au>
2017-06-21 12:08:35 +0800

    added dm function

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :channels, only: [:new, :create, :show, :index]
 
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+
+    resources :direct_messages, only: [:create]
+  end  
+
+
   mount ActionCable.server => '/cable'
 
   root to: 'view#home'
 
   get '/join', to: 'groups#join'
+
+  get 'direct_messages', to: 'conversations#index'
+  
 end
